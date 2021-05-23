b3be91716dbfc050938e7dbd440515307fcb9b72
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-23 11:26:23 -0400

    Changed the class name

--- PP-Platform-Servers-Master/PP-Platform-Spring-Config-Server/src/main/java/jk/pp/platform/servers/spring/configserver/SpringConfigServerMain.java
+++ PP-Platform-Servers-Master/PP-Platform-Spring-Config-Server/src/main/java/jk/pp/platform/servers/spring/configserver/SpringConfigServerMain.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerMain {
+public class SpringConfigServerMain {
 
 	public static void main(String[] args) {
 
-		SpringApplication app = new SpringApplication(ConfigServerMain.class);
+		SpringApplication app = new SpringApplication(SpringConfigServerMain.class);
 		app.setAdditionalProfiles(new String[] { "platform-server-spring-config" });
 
 		app.run(args);
